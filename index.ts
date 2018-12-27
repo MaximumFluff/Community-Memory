82d33f0c36991e98158bd1604a2033793faf0374
MaximumFluff <a1603181@myy.haaga-helia.fi>
2018-12-28 00:39:06 +0200

    Added timers

--- index.ts
+++ index.ts
@@ -26,9 +26,32 @@ function generateCards(data: Event[]) {
     // Reference for div to be inserted into
     const ref: HTMLElement = document.getElementById("cards")!;
     newDiv.className = "card";
-    newDiv.innerHTML = `<p>${data[i].title}</p><p>${data[i].date}</p><a href="${
+    newDiv.innerHTML = `<p>${data[i].title}</p><p id="${i}"></p><a href="${
       data[i].link
     }" target="_blank">Read more</a>`;
+    const interval = setInterval(function() {
+      timer(i, data[i].date);
+    }, 1000);
     ref.appendChild(newDiv);
   }
+}
+
+function timer(divId: any, date: string) {
+  const difference = new Date(
+    new Date(date).getTime() - new Date().getTime()
+  ).getTime();
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  const timeString =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (0 > difference) {
+    const timeString = "Ended";
+  }
+  // Attach to divId
+  const ref: HTMLElement = document.getElementById(`${divId}`)!;
+  ref.innerHTML = timeString;
 }
